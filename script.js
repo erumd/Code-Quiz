5eb35829b63219d1e4a4412e14e3200dcbf2eba3
Erum Dhukka <erumdhukka531@gmail.com>
2020-12-19 00:58:53 -0600

    added questions and js

---
+++ script.js
@@ -0,0 +1,37 @@
+var questions = ["question1", "question2", ""]
+
+// WHat do you use to style your html?
+    // A.CSS
+    // b. JavaScript
+    // c. jQuery
+    //D Github
+
+    question1 =  {
+        title: "what do you use to style your html",
+        answers: ["CSS", "JavaScript", "Jquery", "GitHub"];
+        correctAnswer: "css"
+    }
+
+    question2= {
+        title: "Which is NOT a semantic element?"
+        answers: ["Header", "Navigation", "Aside", "Div"];
+        correctAnswer2: "Div" 
+    }
+
+    question3 = {
+        title: "What are variables used to store?"
+        answers: ["Numbers", "Letters", "Data Values", "Functions"];
+        correctAnswer:"Data Values"
+    }
+
+    question4 = {
+        title: "In conditional statements which words are NOT used?"
+        answer: ["If", "Else", "Else If", "Than"]
+        correctAnswer: "Than"
+    }
+
+    question5= {
+        title: "A function is a block of code designed to perform a particular _____?"
+        answer:["Data", "Task", "Loop","Condition"]
+        correctAnswer: "Task"
+    }
