ec773044b4f474ba569938be829ca11e3f66ee67
Erum Dhukka <erumdhukka531@gmail.com>
2020-12-19 08:54:18 -0600

    click start button, countdown begins

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// start button 
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+var form = document.querySelector('.form');
+var startBtn = document.getElementById('Start');
+
+var timeLeft;
+
+function countdown() {
+    var timeLeft = 60;
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+      // Call the `displayMessage()` function
+      displayMessage();
+    }
+  }, 1000);
+}
+
+
+
+
 var questions = ["question1", "question2", ""]
 
 // WHat do you use to style your html?
